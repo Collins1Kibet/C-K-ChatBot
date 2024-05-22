8fffb3bf83d987234e540fd4e12600b40cfb7822
Collins1Kibet <collinskibet000@gmail.com>
2024-05-22 12:11:38 +0300

    Cobest_Kenya_ChatBot.py

--- Cobest_Kenya_ChatBot.py
+++ Cobest_Kenya_ChatBot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import google.generativeai as gen_ai
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = f"{working_dir}/.env"
+model_path = f"{working_dir}"
 
 load_dotenv(model_path)
 
